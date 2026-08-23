709deeb86c0540d7277575190010738678da2b25
1josepheber-beep <1josepheber@gmail.com>
2026-08-23 01:50:41 -0500

    App Badge remove

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Hatzalah of Houston - service worker */
-var CACHE="hoh-v8";
-var SHELL=["./","./index.html","./proto_index.js","./placement.js"];
+var CACHE="hoh-v9";
+var SHELL=["./","./index.html","./proto_index.js","./placement.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",function(e){
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(SHELL);}).catch(function(){}));
