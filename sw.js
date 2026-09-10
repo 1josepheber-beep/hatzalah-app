c9d11940b02390c89587cb699c342f912c516fee
1josepheber-beep <1josepheber@gmail.com>
2026-09-10 16:41:00 -0500

    poll 5

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    response. Before this split, every deploy emptied the lot, so anyone who
    updated the app started again from nothing and had no offline copy until
    they had re-opened each screen with a signal. */
-var VERSION = "15.5";
+var VERSION = "15.6";
 var SHELL   = "hoh-shell-" + VERSION;
 var DATA    = "hoh-data";          /* deliberately has no version in the name */
 
